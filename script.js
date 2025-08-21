e36d89b875020ea6d78e4dc4b0fc94153ef866de
unknown <lo.leocadio07@gmail.com>
2025-08-21 16:36:21 -0300

    funcao delete

--- script.js
+++ script.js
@@ -6,4 +6,8 @@ function clean(){
     document.querySelector('#display').value = ''
 }
 
+function deleteLast(){
+    const display = document.querySelector('#display')
+    display.value = display.value.slice(0, -1)
+}
 
